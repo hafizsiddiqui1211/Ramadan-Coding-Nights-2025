eaa5e4180e9ddf5b6f495fcd4c1575055fbe70ab
Hafiz Wildan Ahmed Siddiqui <160965922+hafizsiddiqui1211@users.noreply.github.com>
2025-03-12 02:02:01 +0500

    unit_converter.py

--- Unit-Converter/unit_converter.py
+++ Unit-Converter/unit_converter.py
@@ -18,7 +18,7 @@ def convert_units(value, unit_from, unit_to):
         return None
 
 st.set_page_config(page_title="📊 Unit Converter", layout="wide")
-st.title("Unit Converter by Hafiz Siddiqui")
+st.title("📊 Unit Converter by Hafiz Siddiqui")
 st.info("This app converts values between different units 📊")
 value = st.number_input("Enter the value for Conversion:")
 unit_from = st.selectbox(
