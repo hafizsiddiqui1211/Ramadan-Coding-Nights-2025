a96577f994d9873e0e436982be66691ecba65a5f
Hafiz Wildan Ahmed Siddiqui <160965922+hafizsiddiqui1211@users.noreply.github.com>
2025-07-21 02:32:43 +0500

    Update main.py

--- Simple-Chatbot/main.py
+++ Simple-Chatbot/main.py
@@ -1,7 +1,13 @@
-import chainlit as ct
+import chainlit as cl
+import os
 
 
-@ct.on_message
-async def main(message: ct.Message):
+@cl.on_message
+async def main(message: cl.Message):
     response = f"You said {message.content}"
-    await ct.Message(content=response).send()
+    await cl.Message(content=response).send()
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    cl.run("app.py", host="0.0.0.0", port=port, headless=True)
